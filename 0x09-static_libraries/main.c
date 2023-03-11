f02c7fc6754f910f56b9ae8043f958502c651140
AbdelrahmanMohamed9 <abdelrahmanmohammed049@gmail.com>
2023-03-10 17:41:14 -0800

    task 0

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	_puts("\"my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+	return (0);
+}
