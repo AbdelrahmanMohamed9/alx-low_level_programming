c11cc8ee4e93bb1a6b5674acd69e275356fa64ff
AbdelrahmanMohamed9 <AbdelrahmanMohamed9>
2023-04-06 11:56:01 -0700

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,47 +1,23 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include "holberton.h"
+#include "main.h"
 
 /**
-  * binary_to_uint - Converts a binary number to an unsigned int
-  * @b: The binary string to converts
-  *
-  * Return: The positive number converted from a binary
-  */
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: binary string to converts
+ *
+ * Return: positive number converted from a binary
+ */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int len = 0, count = 0, sum = 0;
+	int num;
+	unsigned int dec_value = 0;
 
-	if (b == NULL)
+	if (!b)
 		return (0);
-
-	len = _strlen(b);
-	while (len--)
+	for (num = 0; b[i]; i++)
 	{
-		if (b[len] != 48 && b[len] != 49)
+		if (b[num] < '0' || b[num] > '1')
 			return (0);
-
-		if (b[len] == 49)
-			sum += 1 << count;
-
-		count++;
+		dec_value = 2 * dec_value + (b[num] - '0');
 	}
-
-	return (sum);
-}
-
-/**
-  * _strlen - Returns the length of a string
-  * @s: String to count
-  *
-  * Return: String length
-  */
-int _strlen(const char *s)
-{
-	int c = 0;
-
-	while (s[c])
-		c++;
-
-	return (c);
+	return (dec_value);
 }
