da6f4e523a08d42638e29305aa85f53903190114
AbdelrahmanMohamed9 <AbdelrahmanMohamed9>
2023-04-07 17:09:11 -0700

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,33 +1,35 @@
 #include "main.h"
 
 /**
- * binary_to_uint - converts a binary number to an unsigned int.
- * @b: pointer to a string of 0 and 1 chars.
+ * binary_to_uint - Converts a binary number to an unsigned int.
+ * @b: Pointer to a string containing the binary number.
  *
- * Return: the converted number, or 0 if there is one or more
- * chars in the string b that is not 0 or 1 or if b is NULL.
+ * Return: The converted number, or 0
+ * if there is an invalid character in b or b is NULL.
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int uint_val = 0;
-	int i = 0;
+	unsigned int result = 0;	/* The result of the conversion */
+	int i = 0;	/* Index for iterating through the string */
 
+	/* If b is NULL, return 0 */
 	if (b == NULL)
 		return (0);
 
+	/* Iterate through the string */
 	while (b[i] != '\0')
 	{
-		if (b[i] == '0' || b[i] == '1')
-		{
-			uint_val <<= 1;
-			uint_val += b[i] - '0';
-			i++;
-		}
-		else
-		{
-			return (0);
-		}
+	/* Check if the current character is not '0' or '1' */
+	if (b[i] != '0' && b[i] != '1')
+		return (0);
+
+	/* Shift the result left and add the value of the current bit */
+	result = (result << 1) + (b[i] - '0');
+
+	/* Move to the next character in the string */
+	i++;
 	}
 
-	return (uint_val);
+	/* Return the final result */
+	return (result);
 }
