d07a1724c9015cd496a8c4df472e0f2535ff11f0
AbdelrahmanMohamed9 <AbdelrahmanMohamed9>
2023-04-08 08:47:40 -0700

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -19,7 +19,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 
 	buf = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
+	if (buf == NULL)
 		return (0);
 
 	f = open(filename, O_RDONLY);
