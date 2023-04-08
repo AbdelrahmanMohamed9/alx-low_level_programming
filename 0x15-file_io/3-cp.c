f52e3459e57751e6cc2d17861004e8bc81418926
AbdelrahmanMohamed9 <AbdelrahmanMohamed9>
2023-04-08 09:14:53 -0700

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,21 +1,22 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <fcntl.h>
 #include <unistd.h>
+#include <fcntl.h>
+
+#define BUFFER_SIZE 1024
 
 /**
- * main - copies the content of one file to another
+ * main - Entry point
+ * @argc: Argument count
+ * @argv: Argument vector
  *
- * @argc: the number of arguments passed to the program
- * @argv: an array of pointers to the arguments
- *
- * Return: 0 on success, or the appropriate error code on failure
+ * Return: 0 on success, otherwise an exit code
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int fd_from, fd_to, read_count, write_count, close_count;
-	char buffer[1024];
+	int fd_from, fd_to, rd, wr;
+	char buffer[BUFFER_SIZE];
 
 	if (argc != 3)
 	{
@@ -24,7 +25,6 @@ int main(int argc, char **argv)
 	}
 
 	fd_from = open(argv[1], O_RDONLY);
-
 	if (fd_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
@@ -32,39 +32,39 @@ int main(int argc, char **argv)
 	}
 
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-
 	if (fd_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		close(fd_from);
 		exit(99);
 	}
 
-	while ((read_count = read(fd_from, buffer, 1024)) > 0)
-	{
-		write_count = write(fd_to, buffer, read_count);
-
-		if (write_count == -1 || write_count != read_count)
+	do {
+		rd = read(fd_from, buffer, BUFFER_SIZE);
+		if (rd == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			close(fd_from);
+			close(fd_to);
+			exit(98);
+		}
+		wr = write(fd_to, buffer, rd);
+		if (wr == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close(fd_from);
+			close(fd_to);
 			exit(99);
 		}
-	}
+	} while (rd > 0);
 
-	if (read_count == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-
-	close_count = close(fd_from);
-	if (close_count == -1)
+	if (close(fd_from) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
 		exit(100);
 	}
 
-	close_count = close(fd_to);
-	if (close_count == -1)
+	if (close(fd_to) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
 		exit(100);
