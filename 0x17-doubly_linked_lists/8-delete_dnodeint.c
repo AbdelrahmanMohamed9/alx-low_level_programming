9c7ccb9f9dba31abedc76417f266cf7dffdaf3ce
AbdelrahmanMohamed9 <abdelrahmanmohammed049@gmail.com>
2023-06-15 14:06:21 -0700

    all

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -24,7 +24,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (*head == cr)
 		*head = cr->next;
 	if (cr->next)
-		cr->next->prv = cr->prev;
+		cr->next->prev = cr->prev;
 	if (cr->prev)
 		cr->prev->next = cr->next;
 
