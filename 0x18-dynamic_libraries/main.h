75b080fb101695962fe42290b0046913b638c10d
AbdelrahmanMohamed9 <abdelrahmanmohammed049@gmail.com>
2023-06-19 13:24:07 -0700

    task0

---
+++ 0x18-dynamic_libraries/main.h
@@ -0,0 +1,30 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int _abs(int n);
+int _isupper(int c);
+int _isdigit(int c);
+int _strlen(char *s);
+int _putchar(char c);
+int _islower(int c);
+int _isalpha(int c);
+void _puts(char *s);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+
+
+#endif /* MAIN_H */
